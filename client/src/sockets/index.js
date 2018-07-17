b071db52a1f0ed98f6ba6bbce07e5c1f0ebcf4e7
Chad Martinson <callenmartinson@gmail.com>
2018-07-17 16:27:50 -0400

    socket starting point

--- client/src/sockets/index.js
+++ client/src/sockets/index.js
@@ -2,7 +2,7 @@ import * as types from '../constants/ActionTypes';
 import { populateFactoryList, addNewFactory, updatedFactory, removedFactory } from '../actions';
 
 const setupSocket = (dispatch) => {
-	const socket = new WebSocket('');
+	const socket = new WebSocket('ws://arcane-scrubland-40726.herokuapp.com/');
 	
 	socket.onopen = () => {
 		socket.send(JSON.stringify({
